2f0367b5f7d5381450d2da88599ba599eada4517
XCODE89 <maph0989@gmail.com>
2023-05-30 15:42:05 -0400

    Miguel - Pestañas maquetadas

--- client/components/screens/gastos/gastos.js
+++ client/components/screens/gastos/gastos.js
@@ -1,12 +1,60 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 const Gastos = () => {
     return (
         <View>
-            <TextInput placeholder='Escriba para Busca el Proyecto o sin Proyecto'/>
-            {/* <TextInput placeholder='Escriba para Busca el Proyecto o sin Proyecto'/> */}
-            <Text>Estos son los gastos</Text>
+            <TextInput style={styles.input} placeholder='Busca el Proyecto o sin Proyecto'/>
+            <View style={styles.inputCont}>
+                <TextInput style={styles.input} placeholder='*Buscar anticipo'/>
+                <TextInput style={styles.input} placeholder='$000.000.000.00'/>
+            </View>
+            <TouchableOpacity style={styles.scan}>
+                <Text>Escanear documento...</Text>
+            </TouchableOpacity>
+            <TextInput style={styles.input} placeholder='*Concepto'/>
+            <View style={styles.inputCont}>
+                <TextInput style={styles.input} placeholder='*NIT/CC'/>
+                <TextInput style={styles.input} placeholder='*Nombre'/>
+            </View>
+            <View style={styles.inputCont}>
+                <TextInput style={styles.input} placeholder='*Valor pagado $...'/>
+                <TextInput style={styles.input} placeholder='Valor IVA $...'/>
+            </View>
+            <View style={styles.inputCont}>
+                <TextInput style={styles.input} placeholder='Valor Rete fuente $'/>
+                <TextInput style={styles.input} placeholder='*Fecha recibo DD/MM/AAAA'/>
+            </View>
+            <View style={styles.inputCont}>
+                <TextInput style={styles.input} placeholder='Valor ICA $'/>
+                <TextInput style={styles.input} placeholder='Escriba el Municipio'/>
+            </View>
+            <TextInput style={styles.input} placeholder='Centro de costos...'/>
+            <TouchableOpacity style={styles.scan}>
+                <Text>Confirmar envío...</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    inputCont : {
+        padding: 5,
+        flexDirection: "row"
+    },
+    scan: {
+        backgroundColor: "lightblue",
+        paddingHorizontal: 10,
+        marginVertical: 5,
+        marginHorizontal: 20,
+        height:"20%" ,
+        borderRadius: 4,
+        justifyContent: "center"
+    },
+    input: {
+      paddingHorizontal: 10,
+      backgroundColor: "lightgrey",
+      marginHorizontal: 10,
+      borderRadius: 10
+    },
+  });
 export default Gastos 
